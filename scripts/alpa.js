a0f3aca9cab5db449268b5264107b4e9fa5a4ff8
nokibh <nokibhowlader2003@gmail.com>
2024-02-16 23:21:26 +0600

    added last

--- scripts/alpa.js
+++ scripts/alpa.js
@@ -9,7 +9,20 @@
 //   // console.log(playgroundSection);
 // }
 
+function continueGame() {
+  // step -1:generate a random alphabet
+  const alphabet = getARandomAlphabet();
+  console.log('your random alphabet', alphabet);
+  // set randomly generated alphabet to the screen (show it)
+  const currentAlphabetElement = document.getElementById('current-alphabet');
+  currentAlphabetElement.innerText = alphabet;
+
+  // set background color
+  setBackgroundColorById(alphabet);
+}
+
 function play() {
   hideElementById('home-screen');
   showElementById('play-ground');
+  continueGame();
 }
