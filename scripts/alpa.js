ba33215c8a171b6ff571a29ee8984e35ec0c7e9f
nokibh <nokibhowlader2003@gmail.com>
2024-02-17 17:54:41 +0600

    added new

--- scripts/alpa.js
+++ scripts/alpa.js
@@ -8,6 +8,23 @@
 //   playgroundSection.classList.remove('hidden');
 //   // console.log(playgroundSection);
 // }
+function handleKeyboardKeyUpEvent(event) {
+  const playerPressed = event.key;
+  console.log('player pressed', playerPressed);
+  // get the expected to press
+  const currentAlphabetElement = document.getElementById('current-alphabet');
+  const currentAlphabet = currentAlphabetElement.innerText;
+  const expectedAlphabet = currentAlphabet.toLocaleLowerCase();
+  console.log(playerPressed, expectedAlphabet);
+  // check matched or not
+  if (playerPressed === expectedAlphabet) {
+    console.log('you get a point');
+  } else {
+    console.log('you missed.you lost a life');
+  }
+}
+
+document.addEventListener('keyup', handleKeyboardKeyUpEvent);
 
 function continueGame() {
   // step -1:generate a random alphabet
