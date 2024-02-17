8367e0778930a08dc659d999c958f063c9a9ad00
nokibh <nokibhowlader2003@gmail.com>
2024-02-17 18:42:20 +0600

    added new

--- scripts/alpa.js
+++ scripts/alpa.js
@@ -10,15 +10,18 @@
 // }
 function handleKeyboardKeyUpEvent(event) {
   const playerPressed = event.key;
-  console.log('player pressed', playerPressed);
+
   // get the expected to press
   const currentAlphabetElement = document.getElementById('current-alphabet');
   const currentAlphabet = currentAlphabetElement.innerText;
   const expectedAlphabet = currentAlphabet.toLocaleLowerCase();
-  console.log(playerPressed, expectedAlphabet);
+
   // check matched or not
   if (playerPressed === expectedAlphabet) {
     console.log('you get a point');
+    console.log('you have pressed correctly', expectedAlphabet);
+    removeBackgroundColorById(expectedAlphabet);
+    continueGame();
   } else {
     console.log('you missed.you lost a life');
   }
@@ -29,7 +32,7 @@ document.addEventListener('keyup', handleKeyboardKeyUpEvent);
 function continueGame() {
   // step -1:generate a random alphabet
   const alphabet = getARandomAlphabet();
-  console.log('your random alphabet', alphabet);
+  // console.log('your random alphabet', alphabet);
   // set randomly generated alphabet to the screen (show it)
   const currentAlphabetElement = document.getElementById('current-alphabet');
   currentAlphabetElement.innerText = alphabet;
--- scripts/utility.js
+++ scripts/utility.js
@@ -11,7 +11,10 @@ function setBackgroundColorById(elementId) {
   const element = document.getElementById(elementId);
   element.classList.add('bg-orange-400');
 }
-
+function removeBackgroundColorById(elementId) {
+  const element = document.getElementById(elementId);
+  element.classList.remove('bg-orange-400');
+}
 function getARandomAlphabet() {
   // get or create an alphabet array
   const alphabetString = 'abcdefghijklmnopqrstuvwxyz';
